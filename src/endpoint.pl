611049bdc5bf85eedb97985fb315f9bf2ffb15af
RuanMoura <ruanmoura13@outlook.com>
2022-03-12 19:30:00 -0300

    fix mines generate
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- src/endpoint.pl
+++ src/endpoint.pl
@@ -6,11 +6,17 @@
 
 :- http_handler('/get-mine', get_mine, []).
 
+:- dynamic pos/3.
+
 range(X,X,[X]) :- !.
 range(X,Y,[X|Xs]) :-
     X =< Y,
     Z is X+1,
     range(Z,Y,Xs).
+
+map(Predicate, [H|T]) :-
+    call(Predicate, H),
+    map(Predicate, T).
 
 mine(0,0).
 mine(1,1).
@@ -23,10 +29,18 @@ mine(7,7).
 mine(8,8).
 mine(9,9).
 
-pos(X, Y, V) :- 
-	range(0,9,X), 
-	range(0,9,Y),
-	ifThenElse(mine(X,Y), V is 9, V is 0).
+seed_row(10, _).
+seed_row(N, R) :-
+	S is N+1,
+	ifThenElse(mine(R,N), V = 9, V = 0),
+	assert(pos(R, N, V)),
+	seed_row(S, R).
+
+seed_pos(10).
+seed_pos(N) :-
+	S is N+1,
+	seed_row(0, N),
+	seed_pos(S).
 
 ifThenElse(X,Y,_) :- X,!,Y.
 ifThenElse(_,_,Z) :- Z.
@@ -37,11 +51,11 @@ minesweeper(_Request) :-
 	   [p('Hello, I am using Prolog on Docker!')]).
 
 get_mine(Request) :-
-	http_read_json_dict(Request, DictIn),
-    return_mine(DictIn, DictOut),
+	seed_pos(0),
+    return_mine(DictOut),
     reply_json(DictOut).
 
-return_mine(_{x: X, y: Y}, _{mine: [R0,R1,R2,R3,R4,R5,R6,R7,R8,R9]}) :-
+return_mine(_{mine: [R0,R1,R2,R3,R4,R5,R6,R7,R8,R9]}) :-
 	findall(E, pos(0, _, E), R0),
 	findall(E, pos(1, _, E), R1),
 	findall(E, pos(2, _, E), R2),
