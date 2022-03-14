58d16c56d4457ccad0092f1c254ed8b9ffda14df
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-03-13 23:51:27 -0300

    Format code and fix fetch
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,16 @@
-const field = document.getElementById("field");
+const field = document.getElementById('field')
 
 const getUrl = (url) => {
-    fetch(url).then((response) => {
-        return response.json();
-    }).then((data)=>{
-        console.log(data);
-    }).catch((err)=>{
-        console.log("error: ", err);
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log('error: ', err)
     })
 }
 
-
-const main = ()=>{
-    getUrl('http://localhost:8000/')
-}
-
-main();
+getUrl('http://localhost:8000/get-mine')
